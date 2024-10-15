96e172e9b59b0b6fcfabe55ce7ba1a67f75155b4
Bogdan Istoc <bistoc@drivenets.com>
2024-10-15 21:29:25 +0300

    patch for fetching battery data from a file for remote usage

--- scripts/helpers.sh
+++ scripts/helpers.sh
@@ -13,51 +13,11 @@ is_osx() {
 	[ $(uname) == "Darwin" ]
 }
 
-is_chrome() {
-	chrome="/sys/class/chromeos/cros_ec"
-	if [ -d "$chrome" ]; then
-		return 0
-	else
-		return 1
-	fi
-}
-
-is_wsl() {
-	version=$(</proc/version)
-	if [[ "$version" == *"Microsoft"* || "$version" == *"microsoft"* ]]; then
-		return 0
-	else
-		return 1
-	fi
-}
-
-command_exists() {
-	local command="$1"
-	type "$command" >/dev/null 2>&1
-}
-
 battery_status() {
-	if is_wsl; then
-		local battery
-		battery=$(find /sys/class/power_supply/*/status | tail -n1)
-		awk '{print tolower($0);}' "$battery"
-	elif command_exists "pmset"; then
-		pmset -g batt | awk -F '; *' 'NR==2 { print $2 }'
-	elif command_exists "acpi"; then
-		acpi -b | awk '{gsub(/,/, ""); print tolower($3); exit}'
-	elif command_exists "upower"; then
-		local battery
-		battery=$(upower -e | grep -E 'battery|DisplayDevice'| tail -n1)
-		upower -i $battery | awk '/state/ {print $2}'
-	elif command_exists "termux-battery-status"; then
-		termux-battery-status | jq -r '.status' | awk '{printf("%s%", tolower($1))}'
-	elif command_exists "apm"; then
-		local battery
-		battery=$(apm -a)
-		if [ $battery -eq 0 ]; then
-			echo "discharging"
-		elif [ $battery -eq 1 ]; then
-			echo "charging"
-		fi
+	# pmset -g batt | awk -F '; *' 'NR==2 { print $2 }'
+	if [ -f /tmp/tmux_battery_remote_pmset_out ]; then
+		cat /tmp/tmux_battery_remote_pmset_out | awk -F '; *' 'NR==2 { print $2 }'
+	else
+		echo no status
 	fi
 }
